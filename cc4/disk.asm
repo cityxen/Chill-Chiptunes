312242a10c827fdd643006c823fd22b851c3f836
deadline-cxn <deadline@cityxen.net>
2024-07-17 04:11:35 -0400

    huh

---
+++ cc4/disk.asm
@@ -0,0 +1,9 @@
+.disk [filename="cxnsc3.d64", name="CHIPTUNES 3", id="CXN24" ] {
+[name="BEGGING.SID", type="prg", prgFiles="prg_files/sc-begging.sid.prg" ],
+[name="GHOST_IN_MY_LOAF.SID", type="prg", prgFiles="prg_files/sc-ghost_in_my_loaf.sid.prg" ],
+[name="OLE_MOGENSEN_02.SID", type="prg", prgFiles="prg_files/sc-ole_mogensen_02.sid.prg" ],
+[name="TAEND_ETT_LJUD.SID", type="prg", prgFiles="prg_files/sc-taend_ett_ljud.sid.prg" ],
+[name="UGLY_SONG.SID", type="prg", prgFiles="prg_files/sc-ugly_song.sid.prg" ],
+
+}
+
